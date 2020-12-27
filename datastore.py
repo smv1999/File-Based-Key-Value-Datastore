ab2fabe121651d88577a2b394c0cb51ae286982f
smv1999 <vaidhyanathan.sm@gmail.com>
2020-12-27 17:16:47 +0530

    Added the feature of file handling

--- datastore.py
+++ datastore.py
@@ -10,12 +10,12 @@ MAX_VAL_SIZE = 16384
 class DataStore:
     def __init__(self):
         self.datastore = {}
+
     # Create operation
     # Use syntax : datastore_obj.create(key, value, time_to_live) where time_to_live is optional and is in seconds
-
     def create(self, key, value, time_to_live=0):
         t1 = Thread(target=(self.create_utility), args=(
-            key, value, time_to_live))  # as per the operation
+            key, value, time_to_live))
         t1.daemon = True
         t1.start()
 
@@ -32,6 +32,9 @@ class DataStore:
                             'time_to_live':  time.time() + time_to_live}
                 if len(key) <= 32:
                     self.datastore[key] = data
+                    # output to file
+                    with open("datastore.json", "w") as outfile:
+                        outfile.write(json.dumps(self.datastore))
                 else:
                     print('The keys must be 32 characters in length')
             else:
@@ -40,12 +43,18 @@ class DataStore:
     # Read operation
     # Use syntax : datastore_obj.read(key)
     def read(self, key):
+        # Returned value from the utility method is to be returned by this read() method
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future = executor.submit(self.read_utility, key)
             return future.result()
 
     # Utility method for performing read operation
     def read_utility(self, key):
+        try:
+            with open("datastore.json", "r") as openfile:
+                self.datastore = json.load(openfile)
+        except Exception:
+            print('File not Found.')
         if key not in self.datastore:
             print('error: Given key does not exist in datastore')
             return
@@ -55,7 +64,7 @@ class DataStore:
                 if time.time() < data['time_to_live']:
                     return json.dumps({key: self.datastore[key]['value']})
                 else:
-                    print('error: Time of ' + key + ' has expired')
+                    print('error: Time of key ' + key + ' has expired')
                     return
             else:
                 data = self.datastore[key]
@@ -65,12 +74,17 @@ class DataStore:
     # Use syntax : datastore_obj.delete(key)
     def delete(self, key):
         t3 = Thread(target=(self.delete_utility),
-                    args=(key,))  # as per the operation
+                    args=(key,))
         t3.daemon = True
         t3.start()
-    # Utility method for performing delete operation
 
+    # Utility method for performing delete operation
     def delete_utility(self, key):
+        try:
+            with open("datastore.json", "r") as openfile:
+                self.datastore = json.load(openfile)
+        except Exception:
+            print('File not Found.')
         if key not in self.datastore:
             print('error: Given key does not exist in datastore')
         else:
@@ -78,24 +92,34 @@ class DataStore:
             if data['time_to_live'] != 0:
                 if time.time() < data['time_to_live']:
                     del self.datastore[key]
+                    with open("datastore.json", "w") as outfile:
+                        outfile.write(json.dumps(self.datastore))
                     print('Success : The record with key ' +
                           key + ' is successfully deleted.')
                 else:
-                    print('error: Time of ' + key + ' has expired')
+                    print('error: Time of key ' + key + ' has expired')
             else:
                 del self.datastore[key]
+                with open("datastore.json", "w") as outfile:
+                    outfile.write(json.dumps(self.datastore))
                 print('Success : The record with key ' +
                       key + ' is successfully deleted.')
 
     # Additional Update / Modify operation within its expiry time
     # Use syntax : datastore_obj.update(key, value)
+
     def update(self, key, value):
         t4 = Thread(target=(self.update_utility), args=(
-            key, value))  # as per the operation
+            key, value))
         t4.daemon = True
         t4.start()
 
     def update_utility(self, key, value):
+        try:
+            with open("datastore.json", "r") as openfile:
+                self.datastore = json.load(openfile)
+        except Exception:
+            print('File not Found.')
         if key not in self.datastore:
             print('error: Given key does not exist in datastore')
         else:
@@ -105,14 +129,18 @@ class DataStore:
                     self.datastore[key]['value'] = value
                     print('Success : The record with key ' +
                           key + ' is successfully updated.')
+                    with open("datastore.json", "w") as outfile:
+                        outfile.write(json.dumps(self.datastore))
                     # After updation, read() is called to display the updated value
                     self.read(key)
                 else:
-                    print('error: Time of ' + key +
+                    print('error: Time of key ' + key +
                           ' has expired. So, modify operation cannot be performed.')
             else:
                 self.datastore[key]['value'] = value
                 print('Success : The record with key ' +
                       key + ' is successfully updated.')
+                with open("datastore.json", "w") as outfile:
+                    outfile.write(json.dumps(self.datastore))
                 # After updation, read() is called to display the updated value
                 self.read(key)
--- example.py
+++ example.py
@@ -2,6 +2,16 @@ from datastore import *
 
 ds = DataStore()
 
+# Creation
 ds.create('chennai', 'Marina', 120)
 
+# Read
 print(ds.read('chennai'))
+
+# Delete
+ds.delete('chennai')
+
+# Updation / Modification
+ds.create('New Delhi', 'India Gate', 120)
+
+ds.update('New Delhi', 'Daula Kuan')
